d8526ed13ababb0c0e84de73b071bcbf535be1d2
Dominic Tremblay <dominictremblay24@gmail.com>
2022-07-29 14:34:10 -0400

    breakout reset

---
+++ callback.js
@@ -0,0 +1,12 @@
+// get an array of scrabble words
+const scrabbleWords = require('./scrabble_words');
+
+// create a function that will give me only the 3 letter words
+// the function will ouput only an array with words that contain 3 letter.
+
+
+// create a function that will give me words that start with a specific letter
+// the function will ouput only an array with words start with a specific letter.
+
+
+
--- test.js
+++
@@ -1,8 +0,0 @@
-const filter = function(list, callback) {
-
-callback = null;
-
-}
-
-
-filter([1,2,3,4,5], n => n > 3);
